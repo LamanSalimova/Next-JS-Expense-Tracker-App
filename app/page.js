36e39b028ced82af974d0d055318cfeaf8a2f07f
Laman Salimova <104246181+LemanSelimova@users.noreply.github.com>
2024-01-27 23:34:37 +0400

    add project structure

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-4">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
+        <h1 className="text-4xl p-4 text-center">Expense Tracker</h1>
+      </div>
+    </main>
+  );
+}
